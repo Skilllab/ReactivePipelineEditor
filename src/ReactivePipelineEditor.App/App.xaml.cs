13f0976c1abc734313105003c5b98e4f273fa38a
Oleg Bezyaev <redslide@yandex.ru>
2026-09-13 20:32:28 +0300

    - Создал простое окно - Добавил IHost

--- src/ReactivePipelineEditor.App/App.xaml.cs
+++ src/ReactivePipelineEditor.App/App.xaml.cs
@@ -1,13 +1,42 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace ReactivePipelineEditor.App;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    private IHost? _host;
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        //Обертка над DI-контейнером с жизненным циклом, конфигурацией и логированием.
+        //Одна строка дает все, что иначе пришлось бы писать руками
+        var builder = Host.CreateApplicationBuilder();
+
+        builder.Services.AddSingleton<MainWindow>();
+
+        _host = builder.Build();
+
+        await _host.StartAsync();
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        if (_host is not null)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+        }
+        base.OnExit(e);
+    }
 }
 
